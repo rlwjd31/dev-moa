221547c4e6fb6190f57633335d2387a5821083b4
rlwjd31 <rlwjd31@naver.com>
2023-05-24 03:43:40 +0900

    💄[Style]: remove star in profile page

---
+++ client/src/pages/NewPost.jsx
@@ -0,0 +1,10 @@
+function NewPost() {
+  return (
+    <div className="flex flex-col">
+      <h2> 글 쓰기</h2>
+      <div></div>
+    </div>
+  );
+}
+
+export default NewPost;
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -8,14 +8,13 @@ function UserProfile() {
   return (
     <>
       <Aside
-        title={'마이페이지'}
+        title="마이페이지"
         categories={{
           titles: ['내 프로필', '내 활동'],
           path: [`${BASE_URL}/user/profile`, `${BASE_URL}/user/activities`],
         }}
       />
       <ProfileContent />
-      <Star />
     </>
   );
 }
