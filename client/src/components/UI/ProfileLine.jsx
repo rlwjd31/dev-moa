41a3a739caad1b6399e8f1d2b4ab615c80cc003c
rlwjd31 <rlwjd31@naver.com>
2023-05-24 18:02:48 +0900

    🚀[Refactor]: reusable 컴포넌트 작성

--- client/src/components/UI/ProfileLine.jsx
+++ client/src/components/UI/ProfileLine.jsx
@@ -1,26 +1,25 @@
 import { useState } from 'react';
 import Modal from '../Modal';
 
-function ProfileLine({ title, content }) {
-  const [showModal, setShowModal] = useState(false);
+function ProfileLine({ title, content, className, children }) {
+  // const [showModal, setShowModal] = useState(false);
 
-  const handleOnClose = () => setShowModal(false);
+  // const handleOnClose = () => setShowModal(false);
+  const contentStyle = `w-full flex flex-initial ${className}`;
   return (
-    <div>
-      <div className="w-full my-[34px] flex flex-initial ">
-        <p className="font-medium text-[15px] w-[20%]">{title}</p>
-        <p className="font-normal text-[15px] w-[80%]">{content}</p>
-        <button
+    <div className={contentStyle}>
+      <p className="text-base text-black3 font-medium text-[15px] w-[20%]">{title}</p>
+      <div className="w-full flex justify-start items-center">{children}</div>
+      {/* <button
           type="button"
           onClick={() => setShowModal(true)}
           className="w-[52px] h-[31px] text-[14px] text-black4 text-center font-medium border-[1px] border-solid border-gray4"
         >
           수정
-        </button>
-      </div>
-      <Modal onClose={handleOnClose} visible={showModal} />
+        </button> */}
     </div>
   );
 }
 
 export default ProfileLine;
+// <Modal onClose={handleOnClose} visible={showModal} />
