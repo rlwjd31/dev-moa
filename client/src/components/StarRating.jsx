d702bccbf5930ee14d18e83d013ab4eb71ec2b68
rlwjd31 <rlwjd31@naver.com>
2023-05-24 18:03:17 +0900

    💄[Modify]: star 간격 수정

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -2,13 +2,14 @@ function StarRating({ rating, onStarClick }) {
   const starNum = [1, 2, 3, 4, 5];
 
   return (
-    <div className="flex">
+    <div className="flex gap-2">
       {starNum.map(star => (
         <span
+          role="button"
           key={star}
           onClick={() => onStarClick(star)}
           className={
-            star <= rating ? 'text-[24px] text-[#7199ff]' : 'text-[24px] text-[#c3c3c3]'
+            star <= rating ? 'text-[24px] text-starBlue' : 'text-[24px] text-starWhite'
           }
         >
           &#9733;
